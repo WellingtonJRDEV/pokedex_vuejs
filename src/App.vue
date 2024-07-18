1379243eb64b732b4477624c74e2c72b50788380
Wellington Junior <jwellington752@gmail.com>
2024-07-18 22:06:01 +0100

    adicionando os arquivos no repositorio

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+// Importação dos elementos vue-router
+import { RouterLink, RouterView } from 'vue-router'
+
+// Importação bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importar estilo CSS
+import './complementos/estilos.css'
+
+// Importar componente de navegação
+import BarraNavegacao from './componentes/BarraNavegacao.vue';
+
+</script>
+
+<template>
+  <!-- Barra de navegação -->
+  <BarraNavegacao />
+  <RouterView />
+</template>
